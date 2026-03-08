220937c206fac4bf83853c98a7806e1f91c0ac53
Abhinav Bajpai <Abhinav123bajpai@gmail.com>
2026-03-08 22:18:13 +0530

    fix

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e7681877'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.k0pf1fa9gk8"
+    "revision": "0.f94gv0npa1s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
